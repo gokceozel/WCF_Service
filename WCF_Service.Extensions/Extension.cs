d3fe5c7abd6642bbb7d69c989dca5b054b49359e
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-08-11 01:40:07 +0300

    Extensions

--- WCF_Service.Extensions/Extension.cs
+++ WCF_Service.Extensions/Extension.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WCF_Service.Extensions
 {
-    public class Extension
+    public static class Extension
     {
+        public static T Changer<T> (this object source)
+        {
+            T hedef = Activator.CreateInstance<T>();
+            Type kaynak = source.GetType();
+
+            PropertyInfo[] kaynakProps = kaynak.GetProperties();
+            foreach (var pi in kaynakProps)
+            {
+                object value = pi.GetValue(source);
+                pi.SetValue(hedef, value);
+
+            }
+            return hedef;
+        }
     }
 }
